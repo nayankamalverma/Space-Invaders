b4ef62dc0e1902c73ca91068e1f1f7098ceb468d
nayan <nayankamalverma@gmail.com>
2024-05-21 00:44:44 +0530

    created player class

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,29 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+
+class Player
+{
+    sf::Texture texture;
+    sf::Sprite sprite;
+    int playerScore;
+    int health;
+    int movementSpeed;
+    sf::Vector2f position;
+
+
+public:
+    void takeDamage() {}
+    void move() {}
+    void shootBullets() {}
+};
+
 int main()
 {
     sf::VideoMode const video = *(new sf::VideoMode(1920, 1080));
     
-    sf::RenderWindow* window = new sf::RenderWindow(video,"Hello world",sf::Style::Default);
+    sf::RenderWindow window(video,"Space Invader",sf::Style::Default);
     sf::Font font;
     font.loadFromFile("assets/fonts/OpenSans.ttf");
-    window->setFramerateLimit(60);
+    window.setFramerateLimit(60);
 
 
-    while(window->isOpen())
+    while(window.isOpen())
     {
         sf::Event event;
-        while (window->pollEvent(event)) {
+        while (window.pollEvent(event)) {
             // Check for window closure
             if (event.type == sf::Event::Closed)
-                window->close();
+                window.close();
         }
         
         // Clear the window
-        window->clear();
+        window.clear();
         // Draw your content here
 
         // Display what was drawn
-        window->display();
+        window.display();
     }
     return 0;
 }
