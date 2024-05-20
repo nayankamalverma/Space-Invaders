77c1fac47bbabc822745d7f4c5f72372d016d79b
nayan <nayankamalverma@gmail.com>
2024-05-20 10:18:19 +0530

    added shapes to screen

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -5,6 +5,27 @@ int main()
     
     sf::RenderWindow* window = new sf::RenderWindow(video,"Hello world",sf::Style::Default);
 
+    window->setFramerateLimit(60);
+
+    // Create a green circle
+    sf::CircleShape circle(50);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(100, 100); 
+
+    // Create a red square
+    sf::RectangleShape square(sf::Vector2f(100, 100)); 
+    square.setFillColor(sf::Color::Red);
+    square.setPosition(300, 100);
+
+    // Create a blue triangle
+    sf::ConvexShape triangle;
+    triangle.setPointCount(3);
+    triangle.setPoint(0, sf::Vector2f(0, 100));
+    triangle.setPoint(1, sf::Vector2f(50, 0));
+    triangle.setPoint(2, sf::Vector2f(100, 100));
+    triangle.setFillColor(sf::Color::Blue);
+    triangle.setPosition(500, 100);
+
 
     while(window->isOpen())
     {
@@ -16,8 +37,12 @@ int main()
         }
         
         // Clear the window
-        window->clear(sf::Color::Blue);
+        window->clear();
         // Draw your content here
+        window->draw(circle);
+        window->draw(square);
+        window->draw(triangle);
+
         
         // Display what was drawn
         window->display();
