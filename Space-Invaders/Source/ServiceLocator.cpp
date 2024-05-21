6fb3a845d7c4a1bb38ba49d850ab9efd3ba0e3c7
nayan <nayankamalverma@gmail.com>
2024-05-21 23:26:56 +0530

    added serviceLocator .h and .cpp files

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,22 @@
+#include "../Header/ServiceLocator.h"
+
+ServiceLocator* ServiceLocator::getInstance()
+{
+	//Provides a method to access the unique ServiceLocator instance (object).
+}
+
+void ServiceLocator::initialize()
+{
+	//	Initializes the ServiceLocator.
+}
+
+void ServiceLocator::update()
+{
+	//	Updates all services.
+}
+
+void ServiceLocator::render()
+{
+	//	Renders using the services.
+}
+
