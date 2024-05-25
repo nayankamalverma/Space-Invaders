75a96a64a37eec43593e650ac345b389afb34dda
nayan <nayankamalverma@gmail.com>
2024-05-26 00:23:45 +0530

    updated enemy spawner and added spaw timer

--- Space-Invaders/Source/Enemy/EnemyService.cpp
+++ Space-Invaders/Source/Enemy/EnemyService.cpp
@@ -1,48 +1,60 @@
 #include "../../Header/Enemy/EnemyService.h"
 #include "../../Header/Enemy/EnemyController.h"
+#include "../../Header/Global/ServiceLocator.h"
+#include "../../Header/Time/TimeService.h"
 
 namespace Enemy
 {
-	static EnemyController* spawn;
-	EnemyService::EnemyService()
-	{
-	}
+	using namespace Global;
+	using namespace Time;
 
-	EnemyService::~EnemyService()
-	{
-		destroy();
-	}
+	EnemyService::EnemyService(){}
+
+	EnemyService::~EnemyService() { destroy(); }
 
 	void EnemyService::initialize()
 	{
-		spawn = spawnEnemy();
+		spawn_timer = spawn_interval;
 	}
-
-	EnemyController* EnemyService::spawnEnemy()
-	{
-		EnemyController* enemy = new EnemyController();
-		enemy->initialize();
-
-		return enemy;
-	}
-
 
 	void EnemyService::update()
 	{
-		spawn->update();
+		updateSpawnTimer();
+		processEnemySpawn();
+		for (EnemyController* enemy : enemy_list) enemy->update();
 	}
 
 	void EnemyService::render()
 	{
-		spawn ->render();
+		for (EnemyController* enemy : enemy_list) enemy->render();
 	}
+
+	
+
+	void EnemyService::updateSpawnTimer()
+	{
+		   spawn_timer += ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+	}
+
+	 void EnemyService::processEnemySpawn()
+	 {
+		 if (spawn_timer >= spawn_interval)
+		 {
+			 spawnEnemy();
+			 spawn_timer = 0.0f; 
+		 }
+	 }
+
+	 void EnemyService::spawnEnemy()
+	 {
+		 EnemyController* enemy = new EnemyController();
+		 enemy->initialize();
+
+		 enemy_list.push_back(enemy);
+	 }
 
 	void EnemyService::destroy()
 	{
-		
+		for (EnemyController* enemy : enemy_list) delete(enemy);
 	}
-
-
-
-
 }
