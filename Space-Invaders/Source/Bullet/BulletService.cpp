7040dc1fec87ac46a8c83ca2b44a4d50ffcec404
nayan <nayankamalverma@gmail.com>
2024-05-27 23:14:22 +0530

    created projectile interface

---
+++ Space-Invaders/Source/Bullet/BulletService.cpp
@@ -0,0 +1,27 @@
+#include "../../Header/Bullet/BulletService.h"
+
+BulletService::BulletService()
+{
+	
+}
+
+BulletService::~BulletService()
+{
+	
+}
+
+void BulletService::initialize()
+{
+	
+}
+
+void BulletService::update()
+{
+	
+}
+
+void BulletService::render()
+{
+}
+
+
