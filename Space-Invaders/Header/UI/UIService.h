21203ad9cc357e5193faba1ce54ce6daf29250e7
nayan <nayankamalverma@gmail.com>
2024-05-29 22:08:15 +0530

    Resolved merge conflicts

---
+++ Space-Invaders/Header/UI/UIService.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "../../Header/UI/MainMenu/MainMenuUIController.h"
+#include "../../Header/UI/Interface/IUIController.h"
+
+namespace UI
+{
+	class UIService
+	{
+	private:
+		MainMenu::MainMenuUIController* main_menu_controller;
+
+		void createControllers();
+		void initializeControllers();
+		Interface::IUIController* getCurrentUIController();
+		void destroy();
+
+	public:
+		UIService();
+		~UIService();
+
+		void initialize();
+		void update();
+		void render();
+		void showScreen();
+	};
+}
